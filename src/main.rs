9359db13db5b13a8a19d3eb5521cf656605e6ca3
haaandolo <76086118+haaandolo@users.noreply.github.com>
2023-10-29 20:55:02 +1300

    CFD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+
+use molly_db::wrappers;
+// use molly_db::database_service;
+
+#[tokio::main]
+async fn main() {
+    //GET OHLCV data
+    let eod_client = wrappers::create_reqwest_client().await;
+    let ohlcv = wrappers::get_ohlc(&eod_client, "AAPL", "US", "2023-10-01", "2023-10-15").await;
+    println!("{:#?}", ohlcv.unwrap())
+
+    // INSERT_MANY records into the database
 }
