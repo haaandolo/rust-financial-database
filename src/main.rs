4950682758d72052cb2cea1939848762afd59a11
haaandolo <76086118+haaandolo@users.noreply.github.com>
2023-11-26 13:04:38 +1300

    github workflows test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use molly_db::database_service;
 use molly_db::wrappers;
 
+use env_logger::Env;
 use molly_db::wrappers::OhlcvMetadata;
 use tokio;
-use env_logger::Env;
 
 #[tokio::main]
 async fn main() {
@@ -12,19 +12,41 @@ async fn main() {
     // let request_client = wrappers::create_reqwest_client().await;
 
     // connections
-    let client = database_service::connection().await.expect("Failed to establish connection to DB");
+    let client = database_service::connection()
+        .await
+        .expect("Failed to establish connection to DB");
 
     //GET OHLCV data
     let eod_client = wrappers::create_reqwest_client().await;
-    let ohlcv = wrappers::get_ohlc(&eod_client, "AAPL", "US", "2023-10-01", "2023-11-20").await.unwrap();
+    let ohlcv = wrappers::get_ohlc(&eod_client, "AAPL", "US", "2023-10-01", "2023-11-20")
+        .await
+        .unwrap();
     // println!("{:#?}", &ohlcv);
 
     // INSERT_MANY records into the database
-    let _ = database_service::insert_timeseries(&client, ohlcv, "equity", "spot", "1d", database_service::OhlcGranularity::Hours).await;
+    let _ = database_service::insert_timeseries(
+        &client,
+        ohlcv,
+        "equity",
+        "spot",
+        "1d",
+        database_service::OhlcGranularity::Hours,
+    )
+    .await;
 
     // READ_MANY records from the database
     let metadata: OhlcvMetadata = wrappers::metadata_info().await.unwrap();
-    let records = database_service::read_timeseries(&client, "2023-10-04", "2023-11-01", metadata,"equity", "spot", "1d", database_service::OhlcGranularity::Hours).await;
+    let records = database_service::read_timeseries(
+        &client,
+        "2023-10-04",
+        "2023-11-01",
+        metadata,
+        "equity",
+        "spot",
+        "1d",
+        database_service::OhlcGranularity::Hours,
+    )
+    .await;
     println!("{:#?}", records.unwrap());
 
     // GET TICKER FUNDAMENTALS
@@ -34,7 +56,7 @@ async fn main() {
 
 // change api key to be demo
 // make more apis
-// finishh insert_doc() function and test it 
+// finishh insert_doc() function and test it
 // start read_doc() function
 // change String -> some smart pointer
 // standardise error messages e.g {:#?} or {:?} and format!()
