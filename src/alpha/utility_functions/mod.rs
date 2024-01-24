2643104cb88ce6bc6c501c46c924bfc5b2b996a1
haaandolo <76086118+haaandolo@users.noreply.github.com>
2024-01-24 22:31:16 +1300

    compute_meta_info() almost complete

--- src/alpha/utility_functions/mod.rs
+++ src/alpha/utility_functions/mod.rs
@@ -2,6 +2,7 @@ use crate::database::database_service::MongoDbClient;
 use anyhow::Result;
 use chrono::{Days, NaiveDateTime};
 use polars::{lazy::dsl::col, prelude::*};
+use std::fs::File;
 use tokio;
 
 pub fn get_data() -> Result<Vec<(String, DataFrame)>> {
@@ -79,42 +80,60 @@ impl Alpha {
 
     fn compute_meta_info(self) -> Result<()> {
         let date_range = self.generate_date_range().unwrap();
-        for (_ticker, df) in self.dfs.into_iter() {
+        let mut dfs_updated = Vec::new();
+        for (ticker, df) in self.dfs.into_iter() {
             let daterange_df = df!("datetime" => &date_range).unwrap();
             let df_joined = daterange_df
                 .left_join(&df, ["datetime"], ["datetime"])?
                 .fill_null(FillNullStrategy::Forward(None))?
                 .fill_null(FillNullStrategy::Backward(None))?;
 
-            let df_joined = df_joined
+            let mut df_joined = df_joined
                 .lazy()
                 .with_columns(vec![
                     col("adjusted_close").pct_change(lit(1)).alias("return"),
-                    // col("adjusted_close").shift(lit(1)).alias("shifted"),
                     col("adjusted_close")
                         .map(
                             |s| {
-                                // let rolling_options = RollingOptionsFixedWindow {
-                                //     window_size: 5,
-                                //     min_periods: 5,
-                                //     weights: None,
-                                //     center: false,
-                                //     fn_params: None
-                                // };
-                                let bool_series = &s
+                                let bool_vec = &s
                                     .not_equal(&s.shift(1))?
                                     .into_series()
-                                    .fill_null(FillNullStrategy::Backward(None))?;
-                                // .rolling_sum(rolling_options)?;
-                                Ok(Some(Series::new("", bool_series)))
+                                    .fill_null(FillNullStrategy::Backward(None))?
+                                    .bool()?
+                                    .into_no_null_iter()
+                                    .collect::<Vec<bool>>();
+
+                                // reverse series to get window function to work cos
+                                // window function doesnt do lags. Essentially the
+                                // window function looks forward in time not backward,
+                                // which is why we need to reverse the order
+                                let mut bool_vec = bool_vec
+                                    .iter()
+                                    .rev()
+                                    .cloned()
+                                    .collect::<Vec<bool>>()
+                                    .windows(5)
+                                    .map(|window| if window.contains(&true) { 1 } else { 0 })
+                                    .collect::<Vec<i32>>();
+                                bool_vec.extend_from_slice(&[0; 4]);
+
+                                // unreverse series to get in right order
+                                let bool_vec = bool_vec.iter().rev().cloned().collect::<Vec<i32>>();
+                                Ok(Some(Series::new("", bool_vec)))
                             },
-                            GetOutput::from_type(DataType::Float64),
+                            GetOutput::from_type(DataType::Boolean),
                         )
                         .alias("eligible"),
                 ])
                 .collect()?;
 
-            println!("{:#?}", df_joined);
+            let mut output_file: File = File::create("./out.csv").unwrap();
+            CsvWriter::new(&mut output_file)
+                .finish(&mut df_joined)
+                .unwrap();
+
+            println!("{:#}", &df_joined);
+            dfs_updated.push((ticker, df_joined))
         }
         Ok(())
     }
